4d8959988939ab47ff519cffd27844f1bd5fbcc6
浜田真人 <makoto@H--M.local>
2022-03-11 05:53:39 +0900

    ログの結合って問題を解いた

--- c/log_binds.py
+++ c/log_binds.py
@@ -1,8 +1,15 @@
 # 入力
 n = int(input())
-input_list = []
+input_dic = {}
 for _ in range(n):
-    input_list.append(list(input().split()))
+    (id, string) = input().split()
+    id = int(id)
+    if (id in input_dic):
+        input_dic[id] += string  # 文字列の連結
+    else :
+        input_dic.update({id : string})
 
-# 処理
+# 出力
+for id in input_dic:
+    print(id, input_dic[id])
 
